d67cdc4ff7e012373a508b5ec4c0fafaaea3e953
José Eurípedes <jeferreirajf@gmail.com>
2023-12-12 15:29:11 -0300

    add create-account style

--- z-front/src/components/create-account/index.tsx
+++ z-front/src/components/create-account/index.tsx
@@ -1,53 +1,44 @@
-"use client"
+"use client";
 
-import { Icons } from "../icons"
-import { Button } from "../ui/button"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card"
-import { Input } from "../ui/input"
-import { Label } from "../ui/label"
+import Image from "next/image";
+import { Icons } from "../icons";
+import { Button } from "../ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "../ui/card";
+import ZImage from "../../images/z.png";
 
 export function CreateAccount() {
-  return (
-    <Card>
-      <CardHeader className="space-y-1">
-        <CardTitle className="text-2xl">Create an account</CardTitle>
-        <CardDescription>
-          Enter your email below to create your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="grid grid-cols-2 gap-6">
-          <Button variant="outline">
-            <Icons.gitHub className="mr-2 h-4 w-4" />
-            Github
-          </Button>
-          <Button variant="outline">
-            <Icons.google className="mr-2 h-4 w-4" />
-            Google
-          </Button>
-        </div>
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t" />
-          </div>
-          <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-background px-2 text-muted-foreground">
-              Or continue with
-            </span>
-          </div>
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" />
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full">Create account</Button>
-      </CardFooter>
-    </Card>
-  )
+    return (
+        <Card className="w-full h-max p-2 border-muted-foreground">
+            <CardHeader className="space-y-1">
+                <CardTitle className="text-2xl uppercase">Bem vindo</CardTitle>
+                <CardDescription>
+                    Use sua conta do Github para entrar
+                </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+                <div className="grid grid-cols-1 gap-6">
+                    <Button variant="outline">
+                        <Icons.gitHub className="mr-2 h-6 w-6" />
+                        Github
+                    </Button>
+                </div>
+            </CardContent>
+            <CardFooter>
+                <Image
+                    src={ZImage}
+                    alt="Bem vindo ao Z!"
+                    className="m-auto"
+                    width={375}
+                    height={375}
+                />
+            </CardFooter>
+        </Card>
+    );
 }
