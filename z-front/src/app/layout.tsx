dc2c995a7f5a44eb5a795269ce20c8afb4113379
José Eurípedes <jeferreirajf@gmail.com>
2023-12-12 14:17:17 -0300

    add roboto

--- z-front/src/app/layout.tsx
+++ z-front/src/app/layout.tsx
@@ -1,19 +1,27 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Z :: Sua plataforma social!',
-  description: 'Plataforma social criada como projeto da masterclass do Doutorwaka.com',
-}
+    title: "Z :: Sua plataforma social!",
+    description:
+        "Plataforma social criada como projeto da masterclass do Doutorwaka.com",
+};
+
+const roboto = Roboto({
+    weight: ["100", "400", "700"],
+    subsets: ["latin"],
+    variable: "--font-roboto",
+});
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="pt-BR">
-      <body>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="pt-BR">
+            <body className={roboto.variable}>{children}</body>
+        </html>
+    );
 }
