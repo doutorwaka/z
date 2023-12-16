a0c5a1980484cd68bbc73f86082b374267507873
José Eurípedes <jeferreirajf@gmail.com>
2023-12-16 15:04:54 -0300

    add github authentication button

---
+++ z-front/src/components/create-account/github-auth-button/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Icons } from "@/components/icons";
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+
+export function GithubAuthButton() {
+    return (
+        <div className="grid grid-cols-1 gap-6">
+            <Button variant="outline" onClick={() => signIn("github")}>
+                <Icons.gitHub className="mr-2 h-6 w-6" />
+                Github
+            </Button>
+        </div>
+    );
+}
