06bbfabd5ac52a873c5f78376afa0c0a4df316f7
José Eurípedes <jeferreirajf@gmail.com>
2023-12-16 15:08:09 -0300

    add redirect config

--- z-front/src/app/api/auth/[...nextauth]/route.ts
+++ z-front/src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,19 @@
+import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 
-const authOptions = {
+const authOptions: NextAuthOptions = {
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID || "",
             clientSecret: process.env.GITHUB_SECRET || "",
         }),
     ],
+    callbacks: {
+      async redirect({baseUrl}){
+        return baseUrl + "/home";
+      }
+    }
 };
 
 const handler = NextAuth(authOptions);
