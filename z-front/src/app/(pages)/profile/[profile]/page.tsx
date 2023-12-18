0deeac6f7e976e134e8f2d754ef5e7a3ef3b6fed
José Eurípedes <jeferreirajf@gmail.com>
2023-12-17 21:49:26 -0300

    add useparams into profile page

--- z-front/src/app/(pages)/profile/[profile]/page.tsx
+++ z-front/src/app/(pages)/profile/[profile]/page.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import { Tweet } from "@/components/tweet";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useParams } from "next/navigation";
 
 export default function ProfilePage() {
-    const profile = {
+    const {profile} = useParams();
+
+    console.log("profile", profile);
+
+    const profileTmp = {
         name: "Doutor Waka",
         login: "doutorwaka",
         image: "http://github.com/doutorwaka.png",
@@ -10,16 +17,20 @@ export default function ProfilePage() {
 
     const avatar = (
         <Avatar className="relative top-20 left-4 h-48 w-48 text-[80px]">
-            <AvatarImage src={profile.image} />
-            <AvatarFallback>{profile.name.at(0)?.toUpperCase()}</AvatarFallback>
+            <AvatarImage src={profileTmp.image} />
+            <AvatarFallback>
+                {profileTmp.name.at(0)?.toUpperCase()}
+            </AvatarFallback>
         </Avatar>
     );
 
     return (
         <div className="flex flex-col w-full border-muted border-[1px] gap-2">
             <div className="flex w-full bg-violet-800">{avatar}</div>
-            <div className="mt-20 text-3xl font-bold px-4">{profile.login}</div>
-            <div className="font-light px-4">{profile.name}</div>
+            <div className="mt-20 text-3xl font-bold px-4">
+                {profileTmp.login}
+            </div>
+            <div className="font-light px-4">{profileTmp.name}</div>
 
             <div className="flex gap-8 justify-start px-4 pb-4 border-muted border-b-[1px]">
                 <div className="flex gap-2">
@@ -33,11 +44,11 @@ export default function ProfilePage() {
                 </div>
             </div>
 
-            <Tweet/>
-            <Tweet/>
-            <Tweet/>
-            <Tweet/>
-            <Tweet/>
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
         </div>
     );
 }
